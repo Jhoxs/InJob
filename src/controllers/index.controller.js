080ae74c07836102232847d54744df0c86512d64
José Hernández <joxshernandez@gmail.com>
2022-01-30 22:23:38 -0500

    Update index.controller.js

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -11,7 +11,7 @@ indexCtrl.inicio = async(req, res) => {
         //todos los empleos
         const empleos = await pool.query('SELECT u.nombre, u.apellido,id_empleos, nombre_empleo, sueldo, at.nombre_area, fecha_vencimiento, fecha_registro FROM usuario AS u, empleos AS e, area_trabajo AS at WHERE e.id_area = at.id_area AND u.cedula = id_empresa ORDER BY fecha_registro ASC LIMIT 4');
         //los empleos depende de su valoracion
-        const empVal = await pool.query('SELECT e.*, u.nombre, u.apellido,  SUM(ce.calificacion) AS cal FROM cal_empresa AS ce,usuario AS u, empleos AS e WHERE ce.id_empresa = e.id_empresa AND e.id_empresa = u.cedula GROUP BY (ce.id_empresa) ORDER BY (cal) DESC LIMIT 4');
+        let empVal = await pool.query('SELECT e.*, u.nombre, u.apellido,  SUM(ce.calificacion) AS cal FROM cal_empresa AS ce,usuario AS u, empleos AS e WHERE ce.id_empresa = e.id_empresa AND e.id_empresa = u.cedula GROUP BY (ce.id_empresa) ORDER BY (cal) DESC LIMIT 4');
         if(empVal.length == 0){
             empVal = 0;
         }
