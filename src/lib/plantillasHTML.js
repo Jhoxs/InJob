d02e2819d4a19dae4c92f0c98a37ff3197df1913
José Hernández <joxshernandez@gmail.com>
2022-01-30 20:14:21 -0500

    Update plantillasHTML.js

--- src/lib/plantillasHTML.js
+++ src/lib/plantillasHTML.js
@@ -17,7 +17,7 @@ plantilla.msjReporte = (nombre,comentario,nombre_us,apellido_us) =>{
             <div style="color: #A69595; margin: 4% 10% 2%; text-align: justify;font-family: sans-serif">
                 <h1 style="text-align: center;color:#afa7af; text-decoration: solid;">Notificacion de Reporte</h1>
                 <hr>
-                <h2 style="color: #928492; margin: 0 0 7px">Saludos Empresa ${nombre}.</h2>
+                <h2 style="color: #afa7af; margin: 0 0 7px">Saludos Empresa ${nombre}.</h2>
                 <br>
                 <p style="margin: 2px; font-size: 15px">
                     Este mensaje es para notificarte que tu empresa ha sido reportada.<br><br>
@@ -57,7 +57,7 @@ plantilla.msjAceptacion = (nU,aU,nE,aE,nom_trab) =>{
                 <h1 style="text-align: center;color:#afa7af; text-decoration: solid;">Mensaje de Aceptación</h1>
                 <hr>
                 <br>
-                <h2 style="color: #928492; margin: 0 0 7px">Felicitaciones ${nU} ${aU}.</h2>
+                <h2 style="color: #afa7af; margin: 0 0 7px">Felicitaciones ${nU} ${aU}.</h2>
                 <br>
                 <p style="margin: 2px; font-size: 15px">
                     Este mensaje es para notificarte que usted ha sido seleccionado para formar parte de la empresa ${nE} ${aE}.<br>
